df6c4fb909e329172ca3d3742a4abc3751601377
xiaosean <aa26365566@gmail.com>
2021-05-20 23:56:36 +0800

    Add: scrips for own setting

---
+++ train.sh
@@ -0,0 +1,17 @@
+CUDA_LAUNCH_BLOCKING=1 python scripts/train.py \
+--dataset_type=celebs_sketch_to_face \
+--exp_dir=./experiments/20210520_celeb_sketch_baseline \
+--workers=20 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=2500 \
+--save_interval=5000 \
+--encoder_type=GradualStyleEncoder \
+--start_from_latent_avg \
+--lpips_lambda=0.8 \
+--l2_lambda=1 \
+--id_lambda=0 \
+--w_norm_lambda=0.005 \
+--label_nc=1 \
+--input_nc=1
